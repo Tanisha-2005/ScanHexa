655bf610fb5a61c81ae3d770bbaa86d405d6d1a8
Tanisha-2005 <gchanchal456@gmail.com>
2026-04-17 23:45:31 +0530

    Fix: Added trust proxy setting for Render production environment. This resolves session cookie issues on HTTPS deployments.

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
